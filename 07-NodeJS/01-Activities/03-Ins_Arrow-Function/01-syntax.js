b440a742a4a7cc067176339607f2389e79c95bf7
Avery Jacobson <averyajacobson@gmail.com>
2024-12-30 23:07:01 -0600

    added my arrow function notes

--- 07-NodeJS/01-Activities/03-Ins_Arrow-Function/01-syntax.js
+++ 07-NodeJS/01-Activities/03-Ins_Arrow-Function/01-syntax.js
@@ -1,9 +1,6 @@
 // All of the `createGreeting` functions are equivalent
 
-// Function Expression
-const createGreeting = function (message, name) {
-  return `${message} 1 ${name}!`;
-};
+
 
 // We can safely swap out function expressions with arrow functions most of the time
 const createGreeting2 = (message, name) => {
@@ -13,11 +10,29 @@ const createGreeting2 = (message, name) => {
 // If the arrow function body contains only one expression, we can omit the curly braces and auto return it
 const createGreeting3 = (message, name) => `${message}, ${name}!`;
 
+const greeting2 = createGreeting2("Hi", "Taylor");
+const greeting3 = createGreeting3("Welcome", "Jordan");
+
+
+console.log(greeting2);
+console.log(greeting3);
+
+
+// Function Expression
+const createGreeting = function (message, name) {
+  return `${message} ${name}!`;
+};
+
+
+const greeting1 = createGreeting("Hello", "Avery");
+
+console.log(greeting1);
+
 // If an arrow function only has one parameter, we can omit the parens () around the single parameter
 const greet = (greeting) => console.log(greeting);
 
 // We call arrow functions the same way as we call regular functions
-const greeting = createGreeting('Hello', 'Angie');
+const greeting = createGreeting('Hello to', 'Angie');
 
 // Logs "Hello, Angie!";
 greet(greeting);
